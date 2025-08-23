face63f38f5694d9a2cbb771b18d27124fcc7ffd
hanissiddiq <hanissiddiq10@gmail.com>
2025-08-23 16:44:07 +0700

    add Admin dashboard Project NextJS

---
+++ admin-portal-job/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Button className="">Routes Company Dashboard</Button>
+  </div>    
+  );
+}
